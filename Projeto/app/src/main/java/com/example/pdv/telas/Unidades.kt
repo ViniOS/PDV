90ff68eaffe7cebd6e7f63c4659ed3e87302e801
Caio-Melote <113381237+Caio-Melote@users.noreply.github.com>
2023-06-01 20:37:51 -0300

    Atualização de versões

--- Projeto/app/src/main/java/com/example/pdv/telas/Unidades.kt
+++ Projeto/app/src/main/java/com/example/pdv/telas/Unidades.kt
@@ -1,7 +1,8 @@
-package com.example.pdv
+package com.example.pdv.telas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pdv.R
 
 class unidades : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
