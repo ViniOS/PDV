3dca13e3215fef5b9ce2340c779f67fac8613bfe
Caio-Melote <113381237+Caio-Melote@users.noreply.github.com>
2023-06-12 17:45:18 -0300

    Adicionando dependencias e ordem nas telas para autenticação
    Colocadas dependências para autenticação de usuário e login feitas com email.
    Também foram colocadas em ordem as primeiras telas para se fazer login ao iniciar o aplicativo.

--- Projeto/app/src/main/java/com/example/pdv/telas/Unidades.kt
+++ Projeto/app/src/main/java/com/example/pdv/telas/Unidades.kt
@@ -2,11 +2,25 @@ package com.example.pdv.telas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import com.example.pdv.R
 
-class unidades : AppCompatActivity() {
+class Unidades : AppCompatActivity() {
+    lateinit var nome_unidade: EditText
+    lateinit var cnpj_unidade: EditText
+    lateinit var endereco_unidade: EditText
+    lateinit var botao_cadastro: Button
+    lateinit var checkbox: CheckBox
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_unidades)
+        setContentView(R.layout.activity_unidade)
+
+        nome_unidade = findViewById(R.id.et_TU_nomeUnidade)
+        cnpj_unidade = findViewById(R.id.et_TU_cnpj)
+        endereco_unidade = findViewById(R.id.et_TU_endereco)
+        checkbox = findViewById(R.id.cb_TU_virtual)
+        botao_cadastro = findViewById(R.id.bt_TU_cadastrar)
     }
 }
