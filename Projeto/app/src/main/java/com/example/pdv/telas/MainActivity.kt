18ad29d711cfc1380dd2f37bc068de355fcadfba
Caio-Melote <113381237+Caio-Melote@users.noreply.github.com>
2023-06-12 17:45:18 -0300

    Adicionando dependencias e ordem nas telas para autenticação
    Colocadas dependências para autenticação de usuário e login feitas com email.
    Também foram colocadas em ordem as primeiras telas para se fazer login ao iniciar o aplicativo.

--- Projeto/app/src/main/java/com/example/pdv/telas/MainActivity.kt
+++ Projeto/app/src/main/java/com/example/pdv/telas/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.pdv.telas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.pdv.R
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bt_inicial: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bt_inicial =findViewById(R.id.bt_inicial)
+
+        bt_inicial.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
--- Projeto/app/src/main/java/com/example/pdv/telas/login.kt
+++ Projeto/app/src/main/java/com/example/pdv/telas/login.kt
@@ -1,5 +1,6 @@
 package com.example.pdv.telas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,7 +12,7 @@ class login : AppCompatActivity() {
     lateinit var et_email : EditText
     lateinit var et_senha : EditText
     lateinit var bt_entrar : Button
-    lateinit var bt_esqueci : Button
+    lateinit var bt_cadastrar : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +20,11 @@ class login : AppCompatActivity() {
         et_email = findViewById(R.id.ed_TL_email)
         et_senha = findViewById(R.id.ed_TL_senha)
         bt_entrar = findViewById(R.id.bt_TL_entrar)
-        bt_esqueci = findViewById(R.id.bt_TL_relembrar)
+        bt_cadastrar = findViewById(R.id.bt_TL_cadastro)
+
+        bt_cadastrar.setOnClickListener {
+            val intent = Intent(this, cadastro::class.java)
+            startActivity(intent)
+        }
     }
 }
